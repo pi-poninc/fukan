8697ecaa9ac61b2af231fb3d7b3f072b0d7b6795
samuraikun <tazitaziawawa@gmail.com>
2019-03-05 01:44:49 +0900

    トップ画面を雑に実装

---
+++ layouts/header.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+.fukan-header
+  v-navigation-drawer(v-model='drawer' :mini-variant="miniVariant" :clipped="clipped" fixed app)
+    v-list
+      v-list-tile(v-for='(item, i) in items' :key='i' :to='item.to' router exact)
+        v-list-tile-action
+          v-icon {{ item.icon }}
+        v-list-tile-content
+          v-list-tile-title(v-text='item.title')
+  v-toolbar(:clipped-left='clipped' fixed app)
+    v-toolbar-side-icon(@click='drawer = !drawer')
+    v-toolbar-title.fukan-header__app-title(@click='rootRedirect') FUKAN
+    v-toolbar-title アップロード
+    v-toolbar-title チュートリアル
+    v-toolbar-title 記事作成
+    v-spacer
+    .fukan-header__search-form
+      v-text-field(label='search' placeholder='記事を検索' solo prepend-inner-icon='search')
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      title: 'FUKAN',
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'backup',
+          title: 'アップロード',
+          to: '/upload'
+        }
+      ],
+      miniVariant: false
+    }
+  },
+  methods: {
+    rootRedirect() {
+      console.log('redirect!')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.fukan-header {
+  &__app-title {
+    cursor: pointer;
+  }
+
+  &__search-form {
+    display: flex;
+    justify-content: center;
+    width: 300px;
+  }
+}
+</style>
