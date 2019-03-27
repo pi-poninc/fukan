f13c83a4cbf2b8db83f59753b7a08a1b65443482
samuraikun <tazitaziawawa@gmail.com>
2019-03-28 01:26:30 +0900

    moleculesのeditor-menu-bar コンポーネントを使用する

--- pages/posts/new.vue
+++ pages/posts/new.vue
@@ -2,43 +2,11 @@
 .posts__editor-screen
   .posts__editor-container
     editor-menu-bar(:editor='editor')
-      .menubar(slot-scope='{ commands, isActive }')
-        button.menubar__button(:class="{ 'is-active': isActive.bold() }", @click='commands.bold')
-          v-icon format_bold
-        button.menubar__button(:class="{ 'is-active': isActive.italic() }", @click='commands.italic')
-          v-icon format_italic
-        button.menubar__button(:class="{ 'is-active': isActive.strike() }", @click='commands.strike')
-          v-icon strikethrough_s
-        button.menubar__button(:class="{ 'is-active': isActive.underline() }", @click='commands.underline')
-          v-icon format_underline
-        button.menubar__button(:class="{ 'is-active': isActive.code() }", @click='commands.code')
-          v-icon code
-        button.menubar__button(:class="{ 'is-active': isActive.heading({ level: 1 }) }", @click='commands.heading({ level: 1 })')
-          | H1
-        button.menubar__button(:class="{ 'is-active': isActive.heading({ level: 2 }) }", @click='commands.heading({ level: 2 })')
-          | H2
-        button.menubar__button(:class="{ 'is-active': isActive.heading({ level: 3 }) }", @click='commands.heading({ level: 3 })')
-          | H3
-        button.menubar__button(:class="{ 'is-active': isActive.bullet_list() }", @click='commands.bullet_list')
-          v-icon list
-        button.menubar__button(:class="{ 'is-active': isActive.ordered_list() }", @click='commands.ordered_list')
-          v-icon format_list_numbered
-        button.menubar__button(:class="{ 'is-active': isActive.blockquote() }", @click='commands.blockquote')
-          v-icon format_quote
-        button.menubar__button(:class="{ 'is-active': isActive.code_block() }", @click='commands.code_block')
-          v-icon code
-        button.menubar__button(@click='commands.horizontal_rule')
-          v-icon border_horizontal
-        button.menubar__button(@click='commands.undo')
-          v-icon undo
-        button.menubar__button(@click='commands.redo')
-          v-icon redo
-
     editor-content(:editor='editor')
 </template>
 
 <script>
-import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import { Editor, EditorContent } from 'tiptap'
 import {
   Blockquote,
   CodeBlock,
@@ -58,6 +26,7 @@ import {
   Underline,
   History
 } from 'tiptap-extensions'
+import EditorMenuBar from '~/components/molecules/editor/editor-menu-bar.vue'
 
 export default {
   name: 'PostNew',
