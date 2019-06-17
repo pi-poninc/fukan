ccedef47e4ba2c3d45a4fede0a5111c8c79f8611
samuraikun <tazitaziawawa@gmail.com>
2019-06-18 00:11:11 +0900

    add vue-dropzone

--- pages/upload.vue
+++ pages/upload.vue
@@ -1,3 +1,48 @@
 <template lang="pug">
-h1 Upload CSV
+vue-dropzone(
+  ref="myVueDropzone",
+  id="dropzone",
+  :options='dropzoneOptions'
+)
 </template>
+
+<script>
+import vue2Dropzone from 'vue2-dropzone'
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+import firebase from '@/plugins/firebase'
+
+const storage = firebase.storage()
+const bucketRef = storage.refFromURL(
+  'gs://fukan-232601.appspot.com/csv-uploader'
+)
+console.log(bucketRef)
+
+export default {
+  name: 'UploadFile',
+  components: {
+    'vue-dropzone': vue2Dropzone
+  },
+  data() {
+    return {
+      dropzoneOptions: {
+        url: 'gs://fukan-232601.appspot.com/csv-uploader',
+        thumbnailWidth: 200,
+        addRemoveLinks: false,
+        dictDefaultMessage: "<i class='fa fa-cloud-upload'></i>UPLOAD CSV"
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+#dropzone >>> .dz-message {
+  font-weight: 700;
+  color: #acacac;
+}
+#dropzone >>> .fa-cloud-upload {
+  margin-right: 10px;
+}
+</style>
